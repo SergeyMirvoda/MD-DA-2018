115ab8a72d5a8687a719f38508a92549b8fc7752
Sergey Mirvoda <sergey@mirvoda.com>
2018-09-26 21:19:15 +0500

    Новые лабы

---
+++ classwork4/gmp.R
@@ -0,0 +1,25 @@
+#Загрузите данные в датафрейм. Адрес: github    https://raw???путь_к_файлу_найдите_сами???/data/gmp.dat 
+gmp <- ...
+
+estimate.scaling.exponent <- function(a, y0=6611, response=gmp$pcgmp,
+                                        predictor = gmp$pop, maximum.iterations=100, deriv.step = 1/100,
+                                        step.scale = 1e-12, stopping.deriv = 1/100) {
+  mse <- function(a) { mean((response - y0*predictor^a)^2) }
+  for (iteration in 1:maximum.iterations) {
+    deriv <- (mse(a+deriv.step) - mse(a))/deriv.step
+    a <- a - step.scale*deriv
+    if (abs(deriv) <= stopping.deriv) { break() }
+  }
+  fit <- list(a=a,iterations=iteration,
+              converged=(iteration < maximum.iterations))
+  return(fit)
+}
+#Пример вызова с начальным занчением a
+estimate.scaling.exponent(0.15)
+
+#С помошью полученного коэффициента постройте кривую (функция curve) зависимости
+
+#Удалите точку из набора исходных данных случайным образом, как изменилось статистическая оценка коэффициента a?
+
+#Запустите оценку несколько раз с разных стартовых точек. Как изменилось значение a?
+
